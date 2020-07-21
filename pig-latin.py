f440fe0ec1a5799ba6c1bc98589fb7f39059ba71
hcolina <slushball420@gmail.com>
2020-07-21 09:19:17 -0500

    print statement

--- pig-latin.py
+++ pig-latin.py
@@ -5,4 +5,4 @@ original = raw_input("Enter a word: ")
 if len(original) > 0 and original.isalpha() :
     print ("You chose: ")
 else: 
-    print ("empty")
+    print ("This is not a valid entry.")
