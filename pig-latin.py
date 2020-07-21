2f8360b746d392bf2a3c1cb8b008690266e32a61
hcolina <slushball420@gmail.com>
2020-07-21 09:34:09 -0500

    call 0 index of variable

--- pig-latin.py
+++ pig-latin.py
@@ -5,6 +5,6 @@ original = raw_input("Enter a word: ")
 if len(original) > 0 and original.isalpha() :
     print ("You chose: " + original)
     word = original.lower()
-    first = 
+    first = word[0]
 else: 
     print ("This is not a valid entry.")
