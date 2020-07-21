862ac674a04e0d75030e2b0c268bea8b80eb62ce
hcolina <slushball420@gmail.com>
2020-07-21 09:15:30 -0500

    fix syntax

--- pig-latin.py
+++ pig-latin.py
@@ -3,6 +3,6 @@ print ("Welcome to the Pig Latin Translator!")
 original = raw_input("Enter a word: ")
 
 if len(original) > 0:
-    print "You chose: "
+    print ("You chose: ")
 else: 
     print ("empy")
