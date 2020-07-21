9d32bacbd3f0dd7aa19ff0bf6c27dd469d63258a
hcolina <slushball420@gmail.com>
2020-07-21 09:34:22 -0500

    declare new variable

--- pig-latin.py
+++ pig-latin.py
@@ -6,5 +6,6 @@ if len(original) > 0 and original.isalpha() :
     print ("You chose: " + original)
     word = original.lower()
     first = word[0]
+    new_word = 
 else: 
     print ("This is not a valid entry.")
