0baa75affbe0f261c32f918f2cc01f6f1fd64a49
hcolina <slushball420@gmail.com>
2020-07-21 09:18:27 -0500

    conditional AND

--- pig-latin.py
+++ pig-latin.py
@@ -2,7 +2,7 @@ print ("Welcome to the Pig Latin Translator!")
 
 original = raw_input("Enter a word: ")
 
-if len(original) > 0:
+if len(original) > 0 and :
     print ("You chose: ")
 else: 
     print ("empty")
